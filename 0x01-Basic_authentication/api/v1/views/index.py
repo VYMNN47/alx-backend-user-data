13d1172254dcd2f120af16b90a4f170cc2b7582f
VYMNN47 <ayman.belahsen.4@gmail.com>
2024-09-03 16:56:10 +0100

    api

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """Raises a 401 Error"""
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """Raises a 403 Error"""
+    abort(403)
